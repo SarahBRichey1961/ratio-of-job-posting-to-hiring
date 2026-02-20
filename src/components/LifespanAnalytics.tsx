7cb5e055198ee44c76e8a804b4a147d0ef467658
Deploy User <user@example.com>
2026-02-19 19:35:08 -0800

    Remove unused useEffect from LifespanAnalytics

--- src/components/LifespanAnalytics.tsx
+++ src/components/LifespanAnalytics.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import { LifespanStats, PostingEvent } from '@/lib/lifespanTracking'
 
 interface LifespanCardProps {
