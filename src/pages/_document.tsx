5fb9482c0c72e7d54eb47c22906b5931ffd3f909
Deploy User <user@example.com>
2026-03-06 10:24:50 -0800

    Integrate Google Analytics 4 - Add GA script to _document.tsx and initialize GA with page view tracking in _app.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,9 +2,33 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 export default class MyDocument extends Document {
   render() {
+    const gaId = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID
+
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          {/* Google Analytics 4 */}
+          {gaId && (
+            <>
+              <script
+                async
+                src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`}
+              ></script>
+              <script
+                dangerouslySetInnerHTML={{
+                  __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${gaId}', {
+                      page_path: window.location.pathname,
+                    });
+                  `,
+                }}
+              ></script>
+            </>
+          )}
+        </Head>
         <body>
           <Main />
           <NextScript />
@@ -13,3 +37,4 @@ export default class MyDocument extends Document {
     )
   }
 }
+
